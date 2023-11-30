0da1388b5ec068e18418cb10e8c1830639991fa2
Ken <erlekow1997@gmail.com>
2023-11-30 17:31:47 +0500

    init Project

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                var count by remember {
+                    mutableStateOf(0)
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = count.toString()
+                    )
+                    Row {
+                        Button(onClick = {
+                            count++
+                        }) {
+                            Text(text = "+")
+                        }
+                        Button(onClick = {
+                            count--
+                        }) {
+                            Text(text = "-")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+//
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    ComposeTutorialTheme {
+//        Greeting("Android")
+//    }
+//}
